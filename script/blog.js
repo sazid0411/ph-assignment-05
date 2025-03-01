4a27dfb1f613491d5faa924c31be4ef5c1e8cfa9
sazid0411 <sazidahamed04@gmail.com>
2025-03-02 02:56:57 +0600

    fix blog page

--- script/blog.js
+++ script/blog.js
@@ -1,6 +1,6 @@
 const btnBack = document.getElementById("btn-back");
 
 btnBack.addEventListener("click", function () {
-    window.location.href = "../index.html";
+    window.location.href = "index.html";
 })
 
